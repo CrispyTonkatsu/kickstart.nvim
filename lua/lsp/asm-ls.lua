e219b08492900870a1e3cc190e54e04f1aace4f1
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2026-09-18 09:58:39 -0700

    Added the arm-asm syntax thingy

--- lua/lsp/asm-ls.lua
+++ lua/lsp/asm-ls.lua
@@ -1,13 +1,21 @@
 return {
-  vim.lsp.config('asm-lsp', {
-    cmd = { 'asm-lsp' },
+  {
+    vim.lsp.config('asm-lsp', {
+      cmd = { 'asm-lsp' },
 
-    root_markers = { '.git' },
+      root_markers = { '.git' },
 
-    settings = {
-      filetypes = { 'asm', 's', 'S' }
-    }
-  }),
+      settings = {
+        filetypes = { 'asm', 's', 'S' }
+      }
+    }),
 
-  vim.lsp.enable('asm-lsp', true),
+    vim.lsp.enable('asm-lsp', true),
+  },
+  {
+    'ARM9/arm-syntax-vim',
+    init = function()
+      vim.cmd([[ au VimEnter,BufReadPost *.[sS] setl filetype=arm ]])
+    end
+  },
 }
